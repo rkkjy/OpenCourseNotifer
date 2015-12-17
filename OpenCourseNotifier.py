5ceeb2927f67967c0aa15b66d0331ef0856c3ff2
Ryan Ke <ryanke95@gmail.com>
2015-12-16 20:28:02 -0800

    Open website, enter in CRN, search and scrape html

--- OpenCourseNotifier.py
+++ OpenCourseNotifier.py
@@ -1,19 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver 
-from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-import unittest
+#import unittest
 
-CRN = input("Enter CRN of class: ")
-
-driver = webdriver.Chrome()
-driver.get("https://registrar.ucdavis.edu/courses/search/index.cfm")
-
-
+CRN = input("Enter CRN of class: ") #Obtain CRN from user
 url = "https://registrar.ucdavis.edu/courses/search/index.cfm"
 
+driver = webdriver.Chrome()
+driver.get(url)
+
+enterCRN = driver.find_element_by_id("course_number")
+enterCRN.send_keys(CRN)
+
+searchButton = driver.find_element_by_name("search")
+searchButton.click()
+
 r = requests.get(url)
-soup = BeautifulSoup(r.content, "html.parser")
+data = BeautifulSoup(r.content, "html.parser")
 
 
