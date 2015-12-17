5c75fb8d7aa7ae1944fc63be4fcb5abb3f4624d4
Ryan Ke <ryanke95@gmail.com>
2015-12-16 19:16:21 -0800

    Opens up website

--- OpenCourseNotifier.py
+++ OpenCourseNotifier.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import unittest
+
+CRN = input("Enter CRN of class: ")
+
+driver = webdriver.Chrome()
+driver.get("https://registrar.ucdavis.edu/courses/search/index.cfm")
+
 
 url = "https://registrar.ucdavis.edu/courses/search/index.cfm"
 
 r = requests.get(url)
-soup = BeautifulSoup(r.content)
+soup = BeautifulSoup(r.content, "html.parser")
 
 
