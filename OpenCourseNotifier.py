19c0f0d2ee8c3eb4bf800be7c806a4b0e6e8baa1
Ryan Ke <ryanke95@gmail.com>
2015-12-16 16:59:16 -0800

    Reads in source code

--- OpenCourseNotifier.py
+++ OpenCourseNotifier.py
@@ -1,2 +1,9 @@
-print "Hello World" 
-print "Testing branch"
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://registrar.ucdavis.edu/courses/search/index.cfm"
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content)
+
+
