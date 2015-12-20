1c2d84952f068a46029ee18e0bce185681c0ad3b
Ryan Ke <ryanke95@gmail.com>
2015-12-19 20:15:39 -0800

    Added loop to check periodically

--- OpenCourseNotifier.py
+++ OpenCourseNotifier.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 import time 
 
 #################### Start function definitions ####################
+
+#Opens driver, enters in CRN, and searches 
 def initialize(CRN, url):
 	driver = webdriver.Chrome()
 	driver.get(url)
@@ -21,25 +23,37 @@ def search(driver):
 #pulls data from page, returns space available in the class 
 def pull(driver):
 	data = BeautifulSoup(driver.page_source, "html.parser")
-	for line in data.find_all(id = "courseResultsDiv"): 
+	for line in data.find_all(id = "courseResultsDiv"): #Navigate to correct location 
 		space = line.find_all(title = "View Course Detail")[2].find("em").get_text()
-		space = int(space.strip())
+		space = int(space.strip())	#Strip whitespace and convert to integer 
 		return space 
+
+#notify user about space availability 
+def notify():
+	print "notify"
 
 #################### End function definitions ####################
 
-CRN = input("Enter CRN of class: ") #Obtain CRN from user
 url = "https://registrar.ucdavis.edu/courses/search/index.cfm" #define URL to be class search website
+CRN = input("Enter CRN of class: ") #Obtain CRN from user
+interval = input("How often do you want to check in seconds? ")
+
 driver = initialize(CRN, url)
 
-time.sleep(5)
+time.sleep(5) #Allow adequate time for javascript to execute and generate desired html
 
-spacesAvailable = pull(driver)
+spacesAvailable = pull(driver) #Returns spaces available for the class
 
-if spacesAvailable != 0:
-	print "There is space available in this class"
-else:
-	print "There is no space available in this class"
+while True:						#loop until break 
+	if spacesAvailable != 0:
+		print "There is space available in this class"
+		notify()
+		break 
+	else:
+		time.sleep(interval-5)	#wait given amount of time
+		search(driver)			#click search button again
+		time.sleep(5)
+		spacesAvailable = pull(driver) 	#check updated space availability 
 
 driver.close() #close driver 
 
